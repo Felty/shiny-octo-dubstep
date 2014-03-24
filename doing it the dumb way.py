dd134c3038995a9d2101512ec234530822c45d58
Felty <bstampahar@gmail.com>
2014-03-23 21:01:12 -0400

    bugfixing and some general tidying

--- doing it the dumb way.py
+++ doing it the dumb way.py
@@ -4,8 +4,9 @@ from PIL import ImageDraw
 from random import randint
 import time
 
-xres = 400
-yres = 400
+#4096x4096 for every RGB color
+xres = 80
+yres = 80
 startPos = (int(xres*.5), int(yres *.5))
 
 
@@ -16,15 +17,13 @@ unusedCoords = []
 for x in range(0, xres):
 	for y in range(0, yres):
 		unusedCoords.append((x, y))
-quickc = int(.3*xres)
-unusedColors = []
-#dont forget to change back to 256 after debug
-for r in range(0,256): #just making a list of 1.67 million tuples, I'm sure it'll only take a couple of seconds
-	print "color list at " +str(r)+"/255"
-	for g in range(0,256):
-		for b in range (0, 256):
-			unusedColors.append((r,g,b, 255))
 
+unusedColors = []
+for r in range(0,20): #just making a list of 1.67 million tuples, I'm sure it'll only take a couple of seconds
+	print "color list at " +str(r)+"/255"
+	for g in range(0,20):
+		for b in range (0, 20):
+			unusedColors.append((r,g,b, 255))
 
 def bordering_unused(hubpixel):
 
@@ -55,20 +54,15 @@ unusedCoords.remove(hubpixel)
 trackingBeacon = 0
 timeInWhile = 0
 timeInElse = 0
-timeInMainWhileCheck = 0
-timeInPrinter = 0
-timeInElse1 = 0
-timeInElserand = 0
+timeInTrackingBeacon = 0
 timeInElseDraw = 0
 
 timerstart = time.time()
-mainwhilecheckstart = time.time()
-whiletimerstart = time.time()
 mainLooptimerstart = time.time()
+
 while unusedCoords:
 	mainwhilecheckstop = time.time()
-	timeInMainWhileCheck += (mainwhilecheckstop-mainwhilecheckstart)
-	printertimerstart = time.time()
+	trackingBeaconTimerstart = time.time()
 	trackingBeacon += 1
 	if trackingBeacon > 20:
 		print len(unusedCoords)
@@ -76,8 +70,8 @@ while unusedCoords:
 		trackingBeacon = 0
 		print str(timerstop-timerstart) + " elapsed"
 		timerstart = time.time()
-	printertimestop = time.time()
-	timeInPrinter += printertimestop - printertimerstart
+	trackingBeaconTimerStop = time.time()
+	timeInTrackingBeacon += trackingBeaconTimerStop - trackingBeaconTimerstart
 	whiletimerstart = time.time()	
 	while bordering_unused(hubpixel):
 		while potentialNew:
@@ -93,22 +87,15 @@ while unusedCoords:
 		whiletimerstop = time.time()
 		timeInWhile += whiletimerstop-whiletimerstart
 		elsetimerstart = time.time()
-		else1start = time.time()
 		if len(unusedCoords) == 0:
 			break
-		else1stop = time.time()
-		timeInElse1 += else1stop - else1start
-		elserandstart = time.time()
+		
 		hubpixel = unusedCoords[randint(0, len(unusedCoords)-1)]
-		elserandstop = time.time()
-		timeInElserand += elserandstop - elserandstart
-		#somehow most of the processing time is between this and the next # main culprit is the while loop ending prematurely
 		elsedrawstart = time.time()
 		draw.point(hubpixel, fill=unusedColors[len(unusedColors)-1])
 		unusedColors.remove(unusedColors[len(unusedColors)-1])
 		unusedCoords.remove(hubpixel)
 		elsedrawstop = time.time()
-		#
 		elsetimerstop = time.time()
 		timeInElseDraw += elsedrawstop - elsedrawstart
 		timeInElse += (elsetimerstop-elsetimerstart)
@@ -116,10 +103,7 @@ while unusedCoords:
 mainLooptimerstop = time.time()
 print "seconds in main loop: " + str(mainLooptimerstop - mainLooptimerstart)
 print "seconds in else: " +str(timeInElse)
-print "seconds in else1: "+str(timeInElse1)
-print "seconds in elserand: "+str(timeInElserand)
 print "seconds in elseDraw: "+str(timeInElseDraw)
 print "seconds in in while: "+ str(timeInWhile)
-print "seconds in main while check: "+str(timeInMainWhileCheck)
-print "seconds in printer: "+str(timeInPrinter)
+print "seconds in printer: "+str(timeInTrackingBeacon)
 im.save("result.png")
